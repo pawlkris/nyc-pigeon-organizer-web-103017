196db785117c8ae46634f63d6c32c2d254ecac5d
Paul Kristapovich <paulkristapovich@gmail.com>
2017-10-29 17:21:13 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,16 +2,27 @@ require "pry"
 
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
+  all_names = []
   data.each do |attributetype, hash|
+    hash.each do |attributedata, names|
+      names.each do |x|
+        all_names << x
+      end
+    end
+  end
+  binding.pry
+"""  data.each do |attributetype, hash|
     hash.each do |attributedata, names|
       names.each do |x|
         if pigeon_list.has_key?(x) && pigeon_list[x].has_key(:color)
           pigeon_list[color] << x
-        else pigeon_list.has_key?(x)
+        elsif pigeon_list.has_key?(x)
           pigeon_list[x][attributetype] = [attributedata]
+        else
+          pigeon_list[x] = {}
         end
-    binding.pry
       end
     end
   end
+  """
 end
