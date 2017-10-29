7b8a1d5ca64d252aca12664be7199f98e52386b8
Paul Kristapovich <paulkristapovich@gmail.com>
2017-10-29 17:29:51 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -20,18 +20,11 @@ def nyc_pigeon_organizer(data)
   end
   binding.pry
 
-"""  data.each do |attributetype, hash|
+  data.each do |attributetype, hash|
     hash.each do |attributedata, names|
       names.each do |x|
-        if pigeon_list.has_key?(x) && pigeon_list[x].has_key(:color)
-          pigeon_list[color] << x
-        elsif pigeon_list.has_key?(x)
-          pigeon_list[x][attributetype] = [attributedata]
-        else
-          pigeon_list[x] = {}
-        end
+        pigeon_list[x][attributetype] << [attributedata]
       end
     end
   end
-  """
 end
