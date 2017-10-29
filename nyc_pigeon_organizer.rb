b9c647b49e99f6efe0b80ce24542fc5c31bb35be
Paul Kristapovich <paulkristapovich@gmail.com>
2017-10-29 18:09:42 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -23,7 +23,7 @@ def nyc_pigeon_organizer(data)
   data.each do |attributetype, hash|
     hash.each do |attributedata, names|
       names.each do |x|
-        pigeon_list[x][attributetype] << attributedata.to_str
+        pigeon_list[x][attributetype] << attributedata.to_s
       end
     end
   end
